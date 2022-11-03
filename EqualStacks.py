8e3341d3baddea5645a350c4df4a628f768a3729
Saiksha Desai <104218478+saiksha8502@users.noreply.github.com>
2022-11-03 20:24:48 +0530

    Update EqualStacks.py

--- EqualStacks.py
+++ EqualStacks.py
@@ -4,15 +4,12 @@ def check_equal_heights(heights):
         if h_i != h_0:
             return False
     return True
-
-
 num_blocks = [int(num) for num in input().strip().split()]
 num_stacks = len(num_blocks)
 stacks = []
 for __ in range(num_stacks):
     stack = [int(h) for h in input().strip().split()]
     stacks.append(stack[::-1])
-
 goal = 1
 best_height = 0
 heights = [0] * num_stacks
@@ -21,26 +18,20 @@ total_blocks = sum(num_blocks)
 while sum(positions) < total_blocks:
     for i, stack in enumerate(stacks):
         if positions[i] == num_blocks[i]:
-            continue
-            
+            continue      
         if heights[i] == goal:
             heights[i] += stack[positions[i]]
-            positions[i] += 1
-        
+            positions[i] += 1   
         at_end = False
         while heights[i] < goal:
             if positions[i] == num_blocks[i]:
                 at_end = True
                 break
             heights[i] += stack[positions[i]]
-            positions[i] += 1
-            
-        goal = heights[i]
-            
+            positions[i] += 1        
+        goal = heights[i]      
         if at_end:
-            continue
-            
+            continue      
         if check_equal_heights(heights):
-            best_height = heights[i]
-            
+            best_height = heights[i]      
 print(best_height)
