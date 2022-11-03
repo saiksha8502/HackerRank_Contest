02ee19cbe103245dbb534e8c24f68d45e25f577b
Saiksha Desai <104218478+saiksha8502@users.noreply.github.com>
2022-11-03 20:28:21 +0530

    Update LisasWorbook.c

--- LisasWorbook.c
+++ LisasWorbook.c
@@ -6,22 +6,17 @@ int main() {
     int chapters, probs; scanf("%i %i", &chapters, &probs);
     int *ar = malloc(sizeof(int)*chapters);
     for (int i=0; i<chapters; i++) scanf("%i", &ar[i]);
-    
     int curChapter = 1, curPage = 1, count = 0;
     while (curChapter <= chapters) {
-        
         for (int i=1; i<=ar[curChapter-1]; i++) {
             if (i == curPage)
                 count++;
-            
             if (i+1 <= ar[curChapter-1] && i % probs == 0)
                 curPage++;
         }
-        
         curPage++;
         curChapter++;
     }
-    
     printf("%i", count);
     return 0;
 }
